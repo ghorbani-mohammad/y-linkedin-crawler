3ba5e7fbd21b005f8646887cd2c645802da6f8d3
Mohammad <aali361@gmail.com>
2022-07-24 09:46:02 +0430

    feat: complete login section

--- crawler/linkedin/tasks.py
+++ crawler/linkedin/tasks.py
@@ -1,6 +1,10 @@
+import pickle
 from django.conf import settings
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def login():
@@ -11,3 +15,9 @@ def login():
     password_elem = driver.find_element("id", "password")
     password_elem.send_keys(settings.LINKEDIN_PASSWORD)
     password_elem.submit()
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "global-nav-search"))
+    )
+    pickle.dump(driver.get_cookies(), open("/app/crawler/cookies.pkl", "wb"))
+    driver.quit()
+
