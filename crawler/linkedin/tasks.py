34487719948ce0b3d0b7ec510cf3c0401b019668
Mohammad <aali361@gmail.com>
2022-07-24 11:55:45 +0430

    feat: get feed posts
    Signed-off-by: Mohammad <aali361@gmail.com>

--- crawler/linkedin/tasks.py
+++ crawler/linkedin/tasks.py
@@ -1,4 +1,5 @@
 import pickle
+import time
 from django.conf import settings
 
 from selenium import webdriver
@@ -21,3 +22,21 @@ def login():
     pickle.dump(driver.get_cookies(), open("/app/crawler/cookies.pkl", "wb"))
     driver.quit()
 
+def get_linkedin_feed():
+    driver = webdriver.Remote("http://crawler_firefox:4444/wd/hub", DesiredCapabilities.FIREFOX)
+    cookies = pickle.load(open("/app/crawler/cookies.pkl", "rb"))
+    driver.get("https://www.linkedin.com/")
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    driver.get("https://www.linkedin.com/feed/")
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "global-nav-search"))
+    )
+    articles = driver.find_elements(By.XPATH, ".//div[starts-with(@data-id, 'urn:li:activity:')]")
+    for article in articles:
+        driver.execute_script("arguments[0].scrollIntoView();", article)
+        time.sleep(3)
+        id = article.get_attribute("data-id")
+        body = article.find_element(By.CLASS_NAME, "feed-shared-update-v2__commentary")
+        print(id , body.text)
+    driver.quit()
