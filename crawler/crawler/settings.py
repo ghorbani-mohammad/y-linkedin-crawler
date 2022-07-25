29dc53eba46803a4394e2b8f5e17f0ddfcb14fa8
Mohammad <aali361@gmail.com>
2022-07-25 15:12:26 +0430

    fix: read secret-key from env

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#c=_=og6mm6#l%^jl8!nvj3$ru2*^j&01ke4u9(^&#f-qc4efb'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
