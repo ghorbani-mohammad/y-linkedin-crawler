bc2a880e6a88bedb78e7cff2fe9118701f9f9498
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-25 18:56:27 +0330

    minor change

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -98,8 +98,6 @@ LANGUAGE_CODE = 'en-us'
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
